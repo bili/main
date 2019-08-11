8a1810aa53f83e7ddb65ca16944e033101b85303
bilipan <646740270@qq.com>
2019-08-11 21:30:23 +0800

    docs: 首页

--- client/src/pages/index/App.vue
+++ client/src/pages/index/App.vue
@@ -1,8 +1,14 @@
 <template>
   <section>
     <div class="entry">
-      <router-link to="/channel/乡下大佬">乡下大佬</router-link>
-      <router-link to="/channel/A">A</router-link>
+      <p>公共channels:</p>
+      <p>这里列举了一些常用的channels。</p>
+      <ul>
+        <li v-for="(item, index) in channels" :key="index">
+          <Icon type="ios-chatbubbles" color="#ccc" />
+          <router-link :to="item.link">{{item.name}}</router-link>
+        </li>
+      </ul>
     </div>
     <router-view></router-view>
     <!-- <chat-window channel="乡下大佬"/> -->
@@ -10,9 +16,19 @@
 </template>
 
 <script>
+import { Icon } from "iview";
 export default {
   name: "App",
+  data() {
+    return {
+      channels: [
+        {link: '/channel/乡下大佬', name: '乡下大佬'},
+        {link: '/channel/合家欢', name: '合家欢'},
+      ]
+    }
+  },
   components: {
+    Icon,
     ChatWindow: () => import("@component/ChatWindow")
   },
   mounted() {}
@@ -35,5 +51,20 @@ body {
   top: 0;
   width: 100%;
   height: 100%;
+  padding: 10px;
+  box-sizing: border-box;
+}
+.entry p {
+  font-size: 13px;
+  color: #333;
+}
+.entry li {
+  font-size: 16px;
+}
+.entry li a {
+  color: blue;
+}
+.entry li a:hover {
+  border-bottom: 1px solid blue;
 }
 </style>
